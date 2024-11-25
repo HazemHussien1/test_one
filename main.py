53800a4fad49ab12018d05f1cdd7898830881bd9
hazem <hazemhussien89@gmail.com>
2024-11-25 16:32:01 +0200

    did some todos

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def execute_ping():
     ip = input("Enter an IP address or hostname to ping: ")
     # Vulnerable to command injection
     os.system(f"ping -c 4 {ip}")
+    os.system("ping -c 4 " + ip)
 
 if __name__ == "__main__":
     print("Command Injection Demo")
